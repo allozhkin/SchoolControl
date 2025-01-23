8fc2c32c9bbd801e17d24853d9940dde31cae85a
Roman <romanoff198928@gmail.com>
2025-01-24 00:44:04 +0300

    sprite_modal

---
+++ src/components/ui-kit/Icon/types.ts
@@ -0,0 +1,6 @@
+export type IconProps = {
+  id: string; 
+  width: number;
+  height: number;
+  className?: string; 
+};
--- src/pages/Test/TestPage.tsx
+++ src/pages/Test/TestPage.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import styles from './test.module.scss';
+import Modal from '../../components/modal/Modal';
+import Icon from '../../components/ui-kit/Icon/Icon'; 
 
 const TestPage: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
   return (
     <div className={styles.test__block}>
       <h2 className={styles.test__title}>Test Page </h2>
+      <button onClick={handleOpen} className={styles.test__btn_modal}>Modal open btn</button>
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <h2>Modal content</h2>
+      </Modal>
+    
+      <Icon id="iconHelp-circle" width={24} height={24} className={styles.test__icon} />
+        <Icon id="iconCalendar" width={24} height={24} className={styles.test__icon}/>
+        <Icon id="iconHome" width={24} height={24} className={styles.test__icon}/>
+        <Icon id="iconUser" width={24} height={24} className={styles.test__icon}/>
+        <Icon id="iconSettings" width={24} height={24} className={styles.test__icon}/>
+        <Icon id="iconStatistic" width={24} height={24} className={styles.test__icon}/>
+        <Icon id="iconClose" width={24} height={24} className={styles.test__icon}/>
+        <Icon id="iconDone" width={24} height={24} className={styles.test__icon}/>
     </div>
+
   );
 };
 
