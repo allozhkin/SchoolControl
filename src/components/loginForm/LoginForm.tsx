313fd2c00caf47490fecb310b7606debc12e3e82
Roman <romanoff198928@gmail.com>
2025-01-28 01:33:00 +0300

    fixed27

---
+++ src/components/loginForm/LoginForm.tsx
@@ -0,0 +1,55 @@
+import { useForm } from 'react-hook-form';
+import styles from './login-form.module.scss';
+import Icon from '../ui-kit/Icon/Icon'; 
+
+interface AuthForm {
+  'email': string,
+  'password': string,
+}
+
+const LoginForm = () => {
+  const { register, handleSubmit, formState } = useForm<AuthForm>({
+    mode: 'onChange',
+  })
+
+  const onSubmit = (data:any) => {
+    console.log(data);
+  }
+
+  const emailError = formState.errors.email?.message;
+  const passError = formState.errors.password?.message;
+  return (
+      <>
+        <div className={styles.login__wrapper}>
+          <Icon id="Union" width={48} height={48} />
+          <div className={styles.login_container}>
+            <h1 className={styles.login__title}>SCHOOL CONTROL</h1>
+            <p className={styles.login__text}>Вход</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input className={styles.login__input} type="email" placeholder="Логин" {...register (
+                'email', {
+                  required: 'Введите логин',
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                    message: 'Invalid email address',
+                  },
+                }
+              )}/>
+              {emailError && <p style={{color: 'tomato'}}>{emailError}</p>}
+              <input className={styles.login__input} type="password" placeholder="Пароль"{...register('password', {
+                  required: 'Введите пароль',
+                  minLength: {
+                      value: 6,
+                      message: 'Пароль должен содержать минимум 6 символов'
+                  }
+                })} />
+                {passError && <p style={{color: 'tomato'}}>{passError}</p>}
+              <button className={styles.login__button}>Войти</button>
+            </form>
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default LoginForm;
--- src/pages/Test/TestPage.tsx
+++ src/pages/Test/TestPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './test.module.scss';
 import Modal from '../../components/modal/Modal';
-import Icon from '../../components/ui-kit/Icon/Icon'; 
+import Icon from '../../components/ui-kit/Icon/Icon';
+import LoginForm from '../../components/loginForm/LoginForm';
 
 const TestPage: React.FC = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -18,15 +19,25 @@ const TestPage: React.FC = () => {
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <h2>Modal content</h2>
       </Modal>
-    
+      <span>id="iconHelp-circle"</span>
       <Icon id="iconHelp-circle" width={24} height={24} className={styles.test__icon} />
-        <Icon id="iconCalendar" width={24} height={24} className={styles.test__icon}/>
-        <Icon id="iconHome" width={24} height={24} className={styles.test__icon}/>
-        <Icon id="iconUser" width={24} height={24} className={styles.test__icon}/>
-        <Icon id="iconSettings" width={24} height={24} className={styles.test__icon}/>
-        <Icon id="iconStatistic" width={24} height={24} className={styles.test__icon}/>
-        <Icon id="iconClose" width={24} height={24} className={styles.test__icon}/>
-        <Icon id="iconDone" width={24} height={24} className={styles.test__icon}/>
+      <span>id="iconCalendar"</span>
+      <Icon id="iconCalendar" width={24} height={24} className={styles.test__icon} />
+      <span>id="iconHome"</span>
+      <Icon id="iconHome" width={24} height={24} className={styles.test__icon} />
+      <span>id="iconUser"</span>
+      <Icon id="iconUser" width={24} height={24} className={styles.test__icon} />
+      <span>id="iconSettings"</span>
+      <Icon id="iconSettings" width={24} height={24} className={styles.test__icon} />
+      <span>id="iconStatistic"</span>
+      <Icon id="iconStatistic" width={24} height={24} className={styles.test__icon} />
+      <span>id="iconClose"</span>
+      <Icon id="iconClose" width={24} height={24} className={styles.test__icon} />
+      <span>id="iconDone"</span>
+      <Icon id="iconDone" width={24} height={24} className={styles.test__icon} />
+      <span>id="Union"</span>
+      <Icon id="Union" width={24} height={24} className={styles.test__icon} />
+      <LoginForm />
     </div>
 
   );
