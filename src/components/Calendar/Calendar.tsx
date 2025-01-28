462ae07dfc3bc75e2699ad3d167c426c67df6b20
Kholov Rustam <rustamkholow5@gmail.com>
2025-01-28 20:48:23 +0300

    added style to calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,5 +1,7 @@
 import { memo, useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
