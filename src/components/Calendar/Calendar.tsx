28deb5fd84f0c532dce5b29d97a42e437c90c5d7
Kholov Rustam <rustamkholow5@gmail.com>
2025-02-04 20:53:41 +0300

    Убрал лишний суффикс "г." и сделал первую буквы месяца большой

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -9,9 +9,27 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 function CalendarComponent() {
   const [value, onChange] = useState<Value>(new Date());
 
+  // Функция для форматирования месяца и года
+  const formatMonthYear = (date: Date) => {
+    const monthYear = new Intl.DateTimeFormat('ru-RU', {
+      month: 'long',
+      year: 'numeric',
+    }).format(date);
+
+    // Убираем лишний суффикс "г." и делаем первую буквы месяца большой
+    return monthYear.replace(' г.', '').replace(/^./, (char) => char.toUpperCase());
+  };
+
   return (
     <div className={styles.wrapper}>
-      <Calendar onChange={onChange} value={value} className={styles.calendar} prev2Label={null} next2Label={null} />
+      <Calendar
+        onChange={onChange}
+        value={value}
+        className={styles.calendar}
+        prev2Label={null}
+        next2Label={null}
+        navigationLabel={({ date }) => formatMonthYear(date)} // Форматирование месяца и года
+      />
     </div>
   );
 }
