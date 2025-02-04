07bc06b8a5dffa9d6369115135ad20b143ee3acc
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2025-02-05 02:42:18 +0400

    create PopupContainer and correct FilterClasses

--- src/components/FilterClasses/FilterClasses.tsx
+++ src/components/FilterClasses/FilterClasses.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { classOptions } from './constants';
 import styles from './FilterClasses.module.scss';
 import CheckboxWithLabel from '../ui-kit/checkboxWithLabel/CheckboxWithLabel';
+import PopupContainer from '../ui-kit/popupContainer/popupContainer';
 
 const FilterClasses = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,94 +22,73 @@ const FilterClasses = () => {
 };
 
   return (
-    <div className={styles.filter}>
-      {!isOpen ?
-        <button className={styles.filter__select} onClick={toggleMenu}>
-          Выберите классы
-          <img 
-            className={styles.filter__selectIcon}
-            src='/icons/triangleDown.svg'
-            alt='Dropdown Triangle' />
-        </button>
-      :
-        <>
-          <button className={styles.filter__select_open} onClick={toggleMenu}>
-            Выберите классы
-            <img 
-              className={styles.filter__selectIcon}
-              src='/icons/triangleUp.svg' 
-              alt='Dropdown Triangle' />
-          </button>
-          <div className={styles.filter__wrapper}>
-            <div className={styles.filter__search}>
-              <input 
-                className={styles.filter__searchInput}
-                type="text"
-                placeholder="Поиск"
-              />
-              <img 
-                className={styles.filter__searchIcon} 
-                src='/icons/searchInput.svg' 
-                alt="Search Icon" />
-            </div>
-            <div className={styles.filter__allClasses}>
+    <PopupContainer isOpen={isOpen} name='Выберите классы' onClick={toggleMenu}>
+      <div className={styles.filter__search}>
+        <input 
+          className={styles.filter__searchInput}
+          type="text"
+          placeholder="Поиск"
+        />
+        <img 
+          className={styles.filter__searchIcon} 
+          src='/icons/searchInput.svg' 
+          alt="Search Icon" />
+      </div>
+      <div className={styles.filter__allClasses}>
+        <CheckboxWithLabel
+          label='Все классы'
+          checked={showClasses}
+          onChange={toggleClasses}
+        />
+      </div>
+      {showClasses && (
+        <div className={styles.filter__grades}>
+          {classOptions.map((grade) => (
+            <div className={styles.filter__gradeItem} key={grade.value}>
               <CheckboxWithLabel
-                label='Все классы'
-                checked={showClasses}
-                onChange={toggleClasses}
+                label={grade.label}
+                onChange={() => toggleSection(grade.value)}
+                className={styles.filter__label_level_1}
               />
+              {openSections[grade.value] && (
+                <div>
+                  {grade.children?.map((child) => (
+                    <div key={child.value}>
+                      <CheckboxWithLabel
+                        label={child.label}
+                        onChange={() => toggleSection(child.value)}
+                        className={styles.filter__label_level_2}
+                      />
+                      {openSections[child.value] && (
+                        <div>
+                          {child.children?.map((subChild) => (
+                            <div key={subChild.value}>
+                              <CheckboxWithLabel
+                                label={subChild.label}
+                                onChange={() => toggleSection(subChild.value)}
+                                className={styles.filter__label_level_3}
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
-            {showClasses && (
-              <div className={styles.filter__grades}>
-                {classOptions.map((grade) => (
-                  <div className={styles.filter__gradeItem} key={grade.value}>
-                    <CheckboxWithLabel
-                      label={grade.label}
-                      onChange={() => toggleSection(grade.value)}
-                      className={styles.filter__label_level_1}
-                    />
-                    {openSections[grade.value] && (
-                      <div>
-                        {grade.children?.map((child) => (
-                          <div key={child.value}>
-                            <CheckboxWithLabel
-                              label={child.label}
-                              onChange={() => toggleSection(child.value)}
-                              className={styles.filter__label_level_2}
-                            />
-                            {openSections[child.value] && (
-                              <div>
-                                {child.children?.map((subChild) => (
-                                  <div key={subChild.value}>
-                                    <CheckboxWithLabel
-                                      label={subChild.label}
-                                      onChange={() => toggleSection(subChild.value)}
-                                      className={styles.filter__label_level_3}
-                                    />
-                                  </div>
-                                ))}
-                              </div>
-                            )}
-                          </div>
-                        ))}
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-            <div className={styles.filter__buttons} >
-              <button className={styles.filter__buttonReset} type="submit">
-                Сбросить
-              </button>
-              <button className={styles.filter__buttonApply} type="submit">
-                Применить
-              </button>
-            </div>
-          </div>
-        </>
-      }
-    </div>
+          ))}
+        </div>
+      )}
+      <div className={styles.filter__buttons} >
+        <button className={styles.filter__buttonReset} type="submit">
+          Сбросить
+        </button>
+        <button className={styles.filter__buttonApply} type="submit">
+          Применить
+        </button>
+      </div>
+    </PopupContainer>
   )
 }
 
