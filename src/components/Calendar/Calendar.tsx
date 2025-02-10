0434710a01050cd671413872cee232a5aea24df0
Kholov Rustam <rustamkholow5@gmail.com>
2025-02-10 20:37:40 +0300

    checkpoint

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -9,34 +9,31 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 function CalendarComponent() {
   const [value, onChange] = useState<Value>(new Date());
 
-  // Функция для форматирования месяца и года
   const formatMonthYear = (date: Date) => {
     const monthYear = new Intl.DateTimeFormat('ru-RU', {
       month: 'long',
       year: 'numeric',
     }).format(date);
 
-    // Убираем лишний суффикс "г." и делаем первую буквы месяца большой
     return monthYear.replace(' г.', '').replace(/^./, (char) => char.toUpperCase());
   };
 
   return (
     <div className={styles.wrapper}>
       <h3 className={styles.title}>Выберите дату</h3>
-        {/* Вывод выбранной даты */}
-        <p className={styles.selectedDate}>
-          {value instanceof Date 
-            ? value.toLocaleDateString('ru-RU', { weekday: 'short', day: 'numeric', month: 'long' })
-              .replace(/^./, (char) => char.toUpperCase()) 
-            : 'Не выбрано'}
-        </p>
+      <p className={styles.selectedDate}>
+        {value instanceof Date 
+          ? value.toLocaleDateString('ru-RU', { weekday: 'short', day: 'numeric', month: 'long' })
+            .replace(/^./, (char) => char.toUpperCase()) 
+          : 'Не выбрано'}
+      </p>
       <Calendar
         onChange={onChange}
         value={value}
         className={styles.calendar}
         prev2Label={null}
         next2Label={null}
-        navigationLabel={({ date }) => formatMonthYear(date)} // Форматирование месяца и года
+        navigationLabel={({ date }) => formatMonthYear(date)} 
       />
       
       <div className={styles.button__box}>
